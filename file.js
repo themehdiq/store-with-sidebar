09f8642e4d35f8f43852706249acabf230444a42
themehdiq <qazzih.mehdi97@gmail.com>
2025-10-02 15:55:00 +0100

    work for large and medium screens done

--- file.js
+++ file.js
@@ -1,17 +1,19 @@
-// // Get the button, and when the user clicks on it, execute myFunction
-// document.querySelector(".title").onclick = function () {
-//   myFunction();
-// };
-// document.querySelector(".title1").onclick = function () {
-//   myFunction1();
-// };
+// Get the button, and when the user clicks on it, execute myFunction
+document.querySelector(".category-name-1").onclick = function () {
+  showList1();
+};
+document.querySelector(".category-name-2").onclick = function () {
+  showList2();
+};
 
-// /* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
-// function myFunction() {
-//   document.querySelector(".sub-cat").classList.toggle("show-list");
-//   document.querySelector(".up-arrow").classList.toggle("rotate-arrow");
-// }
-// function myFunction1() {
-//   document.querySelector(".sub-cat1").classList.toggle("show-list");
-//   document.querySelector(".up-arrow1").classList.toggle("rotate-arrow");
-// }
+/* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
+function showList1() {
+  document.querySelector(".menu-1").classList.toggle("show-list");
+  document.querySelector(".list-arrow-1").classList.toggle("rotate-arrow");
+}
+
+/* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
+function showList2() {
+  document.querySelector(".menu-2").classList.toggle("show-list");
+  document.querySelector(".list-arrow-2").classList.toggle("rotate-arrow");
+}
